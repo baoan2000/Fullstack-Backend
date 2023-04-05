f9791352a792c92e59a8fa2fa5179599b51caf01
baoan2000 <121675929+baoan2000@users.noreply.github.com>
2023-04-05 18:23:51 +0700

    Add files via upload

---
+++ Buoi6/main.js
@@ -0,0 +1,56 @@
+const coinprice=
+[
+    {
+      "buy": "66.450",
+      "sell": "67.050",
+      "type": "Vàng SJC 1L - 10L"
+    },
+    {
+      "buy": "54.950",
+      "sell": "55.900",
+      "type": "Vàng nhẫn SJC 99,99 1 chỉ, 2 chỉ, 5 chỉ"
+    },
+    {
+      "buy": "54.950",
+      "sell": "56.000",
+      "type": "Vàng nhẫn SJC 99,99 0,5 chỉ"
+    },
+    {
+      "buy": "54.800",
+      "sell": "55.500",
+      "type": "Vàng nữ trang 99,99%"
+    },
+    {
+      "buy": "53.650",
+      "sell": "54.950",
+      "type": "Vàng nữ trang 99%"
+    },
+    {
+      "buy": "39.779",
+      "sell": "41.779",
+      "type": "Vàng nữ trang 75%"
+    },
+    {
+      "buy": "30.510",
+      "sell": "32.510",
+      "type": "Vàng nữ trang 58,3%"
+    },
+    {
+      "buy": "21.296",
+      "sell": "23.296",
+      "type": "Vàng nữ trang 41,7%"
+    }
+  ];
+let rowinsert=1;
+const gettablecoin = document.getElementById("tablecoin");
+coinprice.forEach((item)=>{
+    const tr=gettablecoin.insertRow(rowinsert);
+const td0=tr.insertCell(0);
+const td1=tr.insertCell(1);
+const td2=tr.insertCell(2);
+td0.innerHTML=item.type;
+td1.innerHTML=item.buy;
+td2.innerHTML=item.sell;
+rowinsert++;
+})
+
